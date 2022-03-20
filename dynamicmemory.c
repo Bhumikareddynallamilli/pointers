d5de68fba26d7fecc24d233e0bf862f97eb9c437
Bhumikareddynallamilli <96302064+Bhumikareddynallamilli@users.noreply.github.com>
2022-03-20 10:10:37 +0530

    Add files via upload

---
+++ dynamicmemory.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int n=5;
+	int *ptr;
+	ptr=(int*)malloc(n*sizeof(int));
+	printf("%d\n",ptr);
+	int i;
+	for(i=0;i<n;i++)
+	{
+		printf("%d %d\n",ptr+i,*(ptr+i));
+	}
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",ptr+i);
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d %d\n",ptr+i,*(ptr+i));
+	}
+}
